7f96a02447e79702f20a27ba10217532faa1b094
martipurull <91315241+martipurull@users.noreply.github.com>
2021-10-07 16:56:40 +0200

    added remove button

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-let addedNames = ['Marti', 'Jean', 'Judit', 'Lara', 'Amandine', 'Angela', 'Paula', 'Lauren']
+let addedNames = []
 
 let numberOfTeams = 0
 
 const addNameToList = function () {
     let inputName = document.getElementById('enterName')
-    addedNames.push(`This is the list of members: ${ inputName.value }`)
+    addedNames.push(inputName.value)
     console.log(addedNames)
     let listOfNames = document.getElementById('memberList')
     let newMemberOnList = document.createElement('li')
+    newMemberOnList.classList.add('list-group-item')
+    newMemberOnList.classList.add('list-group-item-secondary')
     newMemberOnList.innerText = inputName.value
     listOfNames.appendChild(newMemberOnList)
     inputName.value = ''
@@ -15,9 +17,7 @@ const addNameToList = function () {
 
 const setNumberOfTeams = function () {
     let inputNumber = document.getElementById('enterTeamNum')
-    console.log(inputNumber)
     numberOfTeams = inputNumber.value
-    console.log(`This is the number of teams: ${ numberOfTeams }`)
     let teamNumContainer = document.getElementById('teamNum')
     let teamListsContainer = document.createElement('div')
     teamListsContainer.classList.add('row')
@@ -33,21 +33,36 @@ const setNumberOfTeams = function () {
         individualTeamDiv.appendChild(teamName)
         let teamList = document.createElement('ul')
         teamList.setAttribute('id', `team${ i + 1 }`)
+        teamList.classList.add('list-group')
         individualTeamDiv.appendChild(teamList)
+        let removeButton = document.createElement('button')
+        removeButton.innerText = 'Remove Last Member'
+        removeButton.classList.add('btn-sm')
+        removeButton.classList.add('btn-danger')
+        removeButton.addEventListener('click', function () {
+            teamList.removeChild(teamList.lastChild)
+        })
+        individualTeamDiv.appendChild(removeButton)
     }
 }
 
 const assignMembersToTeams = function () {
-    let randomMemberNum = Math.ceil(Math.random() * addedNames.length)
-    let randomTeamNum = Math.ceil(Math.random() * numberOfTeams)
+    if (addedNames.length > 0) {
+        let randomMemberNum = Math.floor(Math.random() * addedNames.length)
+        let randomTeamNum = Math.ceil(Math.random() * numberOfTeams)
+        let randomName = addedNames[randomMemberNum]
+        let randomTeam = document.getElementById(`team${ randomTeamNum }`)
+        let randomNameLi = document.createElement('li')
+        randomNameLi.innerText = randomName
+        randomNameLi.classList.add('list-group-item')
+        randomNameLi.classList.add('list-group-item-success')
+        randomTeam.appendChild(randomNameLi)
+        addedNames.splice(randomMemberNum, 1)
+    } else {
+        alert('ALL MEMBERS HAVE BEEN ASSIGNED TO A TEAM!')
+    }
+}
 
-    let randomName = addedNames[randomMemberNum]
-    let randomTeam = document.getElementById(`team${ randomTeamNum }`)
-    let randomNameLi = document.createElement('li')
-    randomNameLi.innerText = randomName
-    randomTeam.appendChild(randomNameLi)
-    console.log(addedNames)
-    addedNames.splice(randomMemberNum, 1)
-    console.log(addedNames)
+const removeMemberFromTeam = function () {
 
 }
